9e4f86686614e4fd98c839ddebddd6da457a58e6
--global <--global>
2023-06-19 16:29:27 -0600

    Fix not found in reload

--- src/journal/components/Sidebar.tsx
+++ src/journal/components/Sidebar.tsx
@@ -1,9 +1,18 @@
-import { Box, Divider, Drawer, List, Toolbar, Typography, useTheme, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  Toolbar,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
 import { drawerWProps } from "../types/navBarProps";
 import { RootState, useAppDispatch } from "../../store/store";
 import { useSelector } from "react-redux";
 import { SidebarItem } from "./SidebarItem";
-import avatar from "./assets/user-avatar.png";
+import avatar from "/assets/user-avatar.png";
 import { setDrawer, setVariantScreen } from "../../store/journal/journalSlice";
 import { useEffect } from "react";
 
@@ -11,42 +20,45 @@ type variantV = "permanent" | "persistent" | "temporary" | undefined;
 
 export const SideBar = ({ drawerWidth = 240 }: drawerWProps) => {
   const theme = useTheme();
-  const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
   const { displayName, photoURL } = useSelector(
     (state: RootState) => state.auth
   );
-  const { notes,drawerState, variantScreen } = useSelector((state: RootState) => state.journal);
-  const validationI = photoURL!== undefined && photoURL !== null && photoURL.length > 0;
-  
+  const { notes, drawerState, variantScreen } = useSelector(
+    (state: RootState) => state.journal
+  );
+  const validationI =
+    photoURL !== undefined && photoURL !== null && photoURL.length > 0;
+
   const handleDrawerClose = () => {
     dispatch(setDrawer(false));
   };
 
   /* Detect the size of the screen */
-  const variantContent =  variantScreen as variantV
-  const isMediumScreen = useMediaQuery(theme.breakpoints.down('md'));
+  const variantContent = variantScreen as variantV;
+  const isMediumScreen = useMediaQuery(theme.breakpoints.down("md"));
 
   useEffect(() => {
-    if(isMediumScreen){
-      dispatch(setVariantScreen("temporary"))
-      dispatch(setDrawer(false))
-    }else{
-      dispatch(setVariantScreen("persistent"))
-      dispatch(setDrawer(true))
+    if (isMediumScreen) {
+      dispatch(setVariantScreen("temporary"));
+      dispatch(setDrawer(false));
+    } else {
+      dispatch(setVariantScreen("persistent"));
+      dispatch(setDrawer(true));
     }
   }, [isMediumScreen]);
 
   return (
     <Box
       component="nav"
-      sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 }}}
+      sx={{ width: { md: drawerWidth }, flexShrink: { md: 0 } }}
     >
       <Drawer
         open={drawerState}
         onClose={handleDrawerClose}
-        variant={variantContent!}// temporary || permanent
+        variant={variantContent!} // temporary || permanent
         sx={{
-          display: { xs: "flex",  sm: "flex", md: "block", },
+          display: { xs: "flex", sm: "flex", md: "block" },
           "& .MuiDrawer-paper": { boxSizing: "border-box", width: drawerWidth },
         }}
       >
--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,29 +1,30 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import { useCheckAuth } from '../auth/hooks/useCheckAuth'
-import { AuthRoutes } from '../auth/routes/AuthRoutes'
-import { JournalRoutes } from '../journal/routes/JournalRoutes'
-import { CheckingAuth } from '../ui/components/CheckingAuth'
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { useCheckAuth } from "../auth/hooks/useCheckAuth";
+import { AuthRoutes } from "../auth/routes/AuthRoutes";
+import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { CheckingAuth } from "../ui/components/CheckingAuth";
 export const AppRouter = () => {
- 
-  const status = useCheckAuth()
-  if (status === 'checking') {
-    return <CheckingAuth />
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    navigate("/auth/login", { replace: true });
+  }, []);
+
+  const status = useCheckAuth();
+  if (status === "checking") {
+    return <CheckingAuth />;
   }
   return (
     <Routes>
+      {status === "authenticated" ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
 
-      {
-        (status === "authenticated")
-          ? <Route path='/*' element={<JournalRoutes />} />
-          : <Route path='/auth/*' element={<AuthRoutes />} />
-      }
-
-      <Route path='/*' element={<Navigate to='/auth/login' />} />
-
-      {/* Login y pagina de registro */}
-      {/* <Route path='/auth/*' element={<AuthRoutes />} /> */}
-      {/* Jorunal app */}
-      {/* <Route path='/*' element={<JournalRoutes />} /> */}
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
     </Routes>
-  )
-}
+  );
+};
