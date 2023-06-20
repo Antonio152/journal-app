8cfeca5da52c4bfec624f6e17d2a5572e1e40c24
--global <--global>
2023-06-20 15:46:05 -0600

    Fix length of body in Sidebar

--- src/journal/components/SidebarItem.tsx
+++ src/journal/components/SidebarItem.tsx
@@ -1,39 +1,57 @@
-import NoteIcon from '@mui/icons-material/Note';
-import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from '@mui/material'
-import { useMemo } from 'react'
-import { useSelector } from 'react-redux';
-import { restartImagesUpload, setActiveNote, setDrawer } from '../../store/journal/journalSlice'
-import { NoteModel } from '../../store/journal/types/journalSliceTypes'
-import { RootState, useAppDispatch } from '../../store/store'
+import NoteIcon from "@mui/icons-material/Note";
+import {
+  Grid,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import {
+  restartImagesUpload,
+  setActiveNote,
+  setDrawer,
+} from "../../store/journal/journalSlice";
+import { NoteModel } from "../../store/journal/types/journalSliceTypes";
+import { RootState, useAppDispatch } from "../../store/store";
 
-export const SidebarItem = ({ title, id, body, date, imageUrl = [] }: NoteModel) => {
-    const dispatch = useAppDispatch()
-    const { variantScreen } = useSelector((state: RootState) => state.journal);
-    const newTitle = useMemo(() => {
-        return title.length > 17
-            ? title.substring(0, 17) + '...'
-            : title
-    }, [title])
+export const SidebarItem = ({
+  title,
+  id,
+  body,
+  date,
+  imageUrl = [],
+}: NoteModel) => {
+  const dispatch = useAppDispatch();
+  const { variantScreen } = useSelector((state: RootState) => state.journal);
+  const newTitle = useMemo(() => {
+    return title.length > 17 ? title.substring(0, 17) + "..." : title;
+  }, [title]);
 
-    const defineActiveNote = () => {
-        dispatch(setActiveNote({ title, id, body, date, imageUrl }))
-        dispatch(restartImagesUpload())
-        if(variantScreen === "temporary"){
-            dispatch(setDrawer(false))
-        }
+  const newBody = useMemo(() => {
+    return body.length > 20 ? body.substring(0, 20) + "..." : body;
+  }, [body]);
+
+  const defineActiveNote = () => {
+    dispatch(setActiveNote({ title, id, body, date, imageUrl }));
+    dispatch(restartImagesUpload());
+    if (variantScreen === "temporary") {
+      dispatch(setDrawer(false));
     }
+  };
 
-    return (
-        <ListItem key={id} disablePadding >
-            <ListItemButton onClick={()=>defineActiveNote()}>
-                <ListItemIcon sx={{display:"flex",justifyContent:"center" }}>
-                    <NoteIcon />
-                </ListItemIcon>
-                <Grid container>
-                    <ListItemText primary={newTitle} />
-                    <ListItemText secondary={body} />
-                </Grid>
-            </ListItemButton>
-        </ListItem>
-    )
-}
+  return (
+    <ListItem key={id} disablePadding>
+      <ListItemButton onClick={() => defineActiveNote()}>
+        <ListItemIcon sx={{ display: "flex", justifyContent: "center" }}>
+          <NoteIcon />
+        </ListItemIcon>
+        <Grid container>
+          <ListItemText primary={newTitle} />
+          <ListItemText secondary={newBody} />
+        </Grid>
+      </ListItemButton>
+    </ListItem>
+  );
+};
